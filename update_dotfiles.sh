c8b8aca769ef25c0ef668bd5657bb56a5688aafa
Jovyn Tan <jovyn.tls@gmail.com>
2021-12-10 16:48:13 +0800

    add crontabs and shell scripts

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# vim 
 cp ~/.vimrc .
-cp ~/.zshrc .
 cp ~/.vim/UltiSnips/tex.snippets .
+# zsh
+cp ~/.zshrc .
 cp ~/.oh-my-zsh/themes/mh.zsh-theme .
+# editor settings
 cp ~/Library/Application\ Support/Code/User/settings.json ./vscode/
+# daily scripts
+cp -r ~/do-not-move/scripts .
+crontab -l > crontabs
