bffd7b58f975788cab2e15e7b525d3097d51244f
Jovyn Tan <jovyn.tls@gmail.com>
2022-05-15 14:29:47 +0800

    update script

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # vim 
-cp ~/.vimrc .
-cp ~/.vim/UltiSnips/tex.snippets .
+cp ~/.vimrc ./vim
+cp ~/.vim/UltiSnips/tex.snippets ./vim
 cp -r ~/.config/nvim .
 # zsh
-cp ~/.zshrc .
-cp ~/.oh-my-zsh/themes/mh.zsh-theme .
+cp ~/.zshrc ./zsh
+cp ~/.oh-my-zsh/themes/mh.zsh-theme ./zsh
 # kitty
 cp -r ~/.config/kitty .
 # for specifying latex build files
-cp ~/.latexmkrc .
+cp ~/.latexmkrc ./misc
 # editor settings
 cp ~/Library/Application\ Support/Code/User/settings.json ./vscode/
 # obsidian vimrc
-cp ~/Obsidian/obsidian-workspace/.obsidian.vimrc .
+cp ~/Obsidian/obsidian-workspace/.obsidian.vimrc ./misc
 # daily scripts
 cp -r ~/do-not-move/scripts/ ./scripts
 crontab -l > ./scripts/crontabs
