101d68d25a29063e1ea6e40457072010ae7bf7b9
Jovyn Tan <jovyn.tls@gmail.com>
2023-08-18 10:31:44 +0800

    nvim: add fuzzy search in file

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -24,7 +24,7 @@ return {
     config = function()
       -- requires bat and the_silver_searcher
       vim.env.FZF_DEFAULT_COMMAND = 'ag --hidden --ignore="*.git*" --ignore="*.swp" -g ""'
-      vim.env.BAT_THEME = 'Nord'
+      vim.env.BAT_THEME = 'Enki-Light' -- 'Catppuccin-mocha'
       -- change preview pane size
       vim.g.fzf_preview_window = { 'right:60%' }
       vim.g.fzf_layout = { window = { width = 0.9, height = 0.8 } }
@@ -41,12 +41,19 @@ return {
       -- https://github.com/junegunn/fzf.vim/issues/346#issuecomment-655446292
       vim.cmd(
       [[command! -bang -nargs=* Ag call fzf#vim#ag(<q-args>, fzf#vim#with_preview({'options': '--delimiter : --nth 4..'}), <bang>0)]])
+      vim.cmd(
+      [[command! -bang -nargs=* BLinesWithPreview call fzf#vim#grep(
+    \   'rg --with-filename --column --line-number --no-heading --smart-case . '.fnameescape(expand('%:p')), 1,
+    \   fzf#vim#with_preview({'options': '--layout reverse --query '.shellescape(<q-args>).' --with-nth=4.. --delimiter=":"'}))
+      ]]
+      )
       -- Fuzzy finds
       vim.keymap.set('n', '<leader>f', ':Files<Space>')
       vim.keymap.set('n', '<leader>ff', ':Files<CR>')
       vim.keymap.set('n', '<leader>fg', ':GFiles<CR>')
       vim.keymap.set('n', '<leader>fa', ':Ag<CR>')
       vim.keymap.set('n', '<leader>fb', ':Buffers<CR>')
+      vim.keymap.set('n', '<leader>fs', ':BLinesWithPreview<CR>')
     end
   },
 
