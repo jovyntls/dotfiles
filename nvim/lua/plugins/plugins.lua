5a9ea18c3c845ae6cd3adbb4513e074ea408dc92
Jovyn Tan <jovyn.tls@gmail.com>
2024-07-03 17:04:48 +0800

    remove plugins.lua
    left out previously, should have been removed ages ago

--- nvim/lua/plugins/plugins.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- language-specific
-  'lervag/vimtex',
-  { 'sirver/ultisnips', ft = { 'tex' } },
-  'pangloss/vim-javascript',
-  'MaxMEllon/vim-jsx-pretty',
-  -- LSP
-  { 'neoclide/coc.nvim', branch = 'release' }
-}
